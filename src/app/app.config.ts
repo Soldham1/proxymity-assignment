f30eb9f88b31f25d2fc76784da4312c238fd22f0
Soldham1 <s.oldham1204@gmail.com>
2024-10-29 12:15:19 +0000

    fix: get user data

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+function initializeUserData(httpClient: HttpClient): () => Observable<any> {
+  return () =>
+    httpClient
+      .get('https://jsonplaceholder.typicode.com/users')
+      .pipe(tap(console.log));
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeUserData,
+      multi: true,
+      deps: [HttpClient],
+    },
+  ],
 };
